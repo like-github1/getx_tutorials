1b9f0a3f7e02b1ed512c531cb99253b5ccf3e99d
Solomon Girma <likeprogramers@gmail.com>
2022-07-03 07:56:10 -0700

    Named Route using GetX
    Named Route using GetX

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,11 @@ import 'package:get/get.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:getx_tutorials/Screens/Nextpage.dart';
 import 'package:getx_tutorials/Screens/homepage.dart';
-void main(){
 
-  
+void main() {
   runApp(Myapp());
 }
+
 class Myapp extends StatelessWidget {
   const Myapp({Key? key}) : super(key: key);
 
@@ -18,28 +18,24 @@ class Myapp extends StatelessWidget {
       initialRoute: "/",
       defaultTransition: Transition.zoom,
       getPages: [
-        GetPage(name: "/", page:()=> Myapp(),
-         transition: Transition.zoom,
-        ),
-        GetPage(name: "/homepage", page: ()=>homepage()),
-        GetPage(name: "/NextPage", page: ()=>NextPage()),
-       
+        // GetPage(name: "/", page:()=> Myapp(),
+        //  transition: Transition.zoom,
+        // ),
+        GetPage(name: "/homepages", page: () => homepage()),
+        GetPage(name: "/NextPage", page: () => NextPage()),
       ],
       home: Scaffold(
-        appBar: AppBar(title: Text("Named route using Getx Librery"),
-        
-        ),
-        body: Container(
-          child: ElevatedButton(onPressed: (){
-            Get.toNamed(
-             
-            "/homepage ? channels=this is data pass to homepage & content= this is content data too"
-          
-            );
-
-          }, child: Text("Go to home page")),
-        )
-      ),
+          appBar: AppBar(
+            title: Text("Named route using Getx Librery"),
+          ),
+          body: Container(
+            child: ElevatedButton(
+                onPressed: () {
+                  Get.toNamed(
+                      "/homepages ? channels=this is data pass to homepage & content= this is content data too");
+                },
+                child: Text("Go to home page")),
+          )),
     );
   }
 }
