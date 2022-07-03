4574992eeadac719f72c68b368e10bb2542e8953
Solomon Girma <likeprogramers@gmail.com>
2022-07-03 09:20:06 -0700

    Reactive State management
    Reactive State management

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-void main(){
 
-runApp(Myapp());
+void main() {
+  runApp(Myapp());
 }
+
 class Myapp extends StatelessWidget {
-   Myapp({Key? key}) : super(key: key);
- var count=0.obs; //observable or Reactive Variables obs(()=>q)
- void increment(){
-  count++;
- }
+  Myapp({Key? key}) : super(key: key);
+  var count = 0.obs; //observable or Reactive Variables obs(()=>q)
+  void increment() {
+    count++;
+  }
 
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
       title: "Getx State management",
       home: Scaffold(
-        appBar: AppBar(title: Text("Getx Reactive State management"),
+        appBar: AppBar(
+          title: Text("Getx Reactive State management"),
         ),
         body: Center(
           child: Column(
             children: [
-              Obx(()=>Text(" the counter value is $count")),
-              ElevatedButton(onPressed: (){
-                increment();
-
-              }, 
-              child: Text("Increment")
-              )
+              Obx(() => Text(" the counter value is $count")),
+              ElevatedButton(
+                  onPressed: () {
+                    increment();
+                  },
+                  child: Text("Increment"))
             ],
           ),
         ),
