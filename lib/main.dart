3fd777d0281529fb999019526fa237d86ab8e636
Solomon Girma <likeprogramers@gmail.com>
2022-07-04 03:37:25 -0700

    Flutter GetX Tutorial || State Management GetX - by using Obx and Custom Class
    Flutter GetX Tutorial || State Management GetX - by using Obx and Custom Class

--- lib/main.dart
+++ lib/main.dart
@@ -1,68 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/get_core.dart';
 import 'package:get/get_navigation/get_navigation.dart';
-
-void main() {
-  runApp(
-    myapp(),
-  );
+import 'package:getx_tutorials/user.dart';
+void main(){
+  runApp(Myapp());
 }
 
-class myapp extends StatefulWidget {
-  myapp({Key? key}) : super(key: key);
+class Myapp extends StatelessWidget {
+   Myapp({Key? key}) : super(key: key);
+  var users=user(name: "sami",age: 24).obs; //pass Reactive Variables to User Classs
 
-  @override
-  State<myapp> createState() => _myappState();
-}
-
-class _myappState extends State<myapp> {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: "Snackbar using GetX",
+      title: "State anagemnt Using GetX by obs and Custom Class",
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Snackbar"),
-        ),
-        body: Container(
-          child: Center(
-            child: ElevatedButton(
-                onPressed: () {
-                  Get.snackbar(
-                    "snakbar Text", 
-                    "Snackbar Messages",
-                    titleText: const Text(
-                      "this is Snakbar",
-                      style: TextStyle(color: Colors.blue),
-                    ),
-                    messageText: const Text("this is message text"),
-                    colorText: Colors.red,
-                    backgroundColor: Colors.pink,
-                    snackPosition: SnackPosition.BOTTOM,
-                    borderRadius: 2,
-                    margin: const EdgeInsets.all(3),
-                    backgroundGradient: const LinearGradient(colors: [
-                      Colors.red,Colors.blue,
-                    ]),
-                    isDismissible: true,
-                    dismissDirection: DismissDirection.horizontal,
-                    forwardAnimationCurve: Curves.bounceInOut,
-                    //add like leadding icon on Snackbar
-                    icon: Icon(Icons.send, color: Colors.blue,),
-                    shouldIconPulse: false,
-                    //add Trailing Icon on SnackBar
-                    mainButton: TextButton(onPressed: (){}, child: Text("send")),
-                    onTap: (Value){
-                      print("SnackBar is Clicked");
-
-                    }
-
-
-                    );
+        appBar: AppBar(title: Text("Getx State management"),),
+        body: Column(
+          children: [
+            Obx(()=>Text(users.value.name)),
+            Center(
+              child: ElevatedButton(
+                onPressed: (){
+                  users.update((val) {
+                    val!.name=val.name.toString().toUpperCase();
+                    
+                  });
                 },
-                child: const Text("Show SnackBar")),
-          ),
+               child: Text("UpperCase")
+               ),
+            ),
+          ],
         ),
       ),
     );
---
+++ lib/user.dart
@@ -0,0 +1,10 @@
+import 'package:get/get.dart';
+
+
+
+class user{
+  var name;
+  // ignore: prefer_typing_uninitialized_variables
+  var age;
+  user({this.name,this.age});
+}
